da73c9ff67fe593cca34f25c658f8ddf9512d2d4
Dhaval Joshi <joshi.amival@gmail.com>
2020-04-11 13:32:07 -0400

    latest

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World Version latest";
+		return "Hello World Version latest 1";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
